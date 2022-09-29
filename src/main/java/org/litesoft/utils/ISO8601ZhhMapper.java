cbe0f76ba141c9eb42329f9b3279f2a2271dd084
george.smith <litesoft@gmail.com>
2022-09-29 07:37:03 -0700

    Add static methods for lambda support, and Bump Version

--- src/main/java/org/litesoft/utils/ISO8601ZhhMapper.java
+++ src/main/java/org/litesoft/utils/ISO8601ZhhMapper.java
@@ -1,14 +1,33 @@
 package org.litesoft.utils;
 
+import java.time.Instant;
+
 /**
  * Mapper that bidirectionally converts a ISO-8601(ish) timestamp String to/from an Instant with timestamp resolution of: Hour
  * <p>
  * Note: All String outputs are in Zulu/UTC and conforms with the rules defined in <code>ISO8601ZtimeStamp</code>!
  */
+@SuppressWarnings("unused")
 public class ISO8601ZhhMapper extends AbstractISO8601Zmapper {
     public static final ISO8601ZhhMapper INSTANCE = new ISO8601ZhhMapper();
 
     private ISO8601ZhhMapper() {
         super( ISO8601ZtimeStamp::toHour );
+    }
+
+    public static String instantToString( Instant instant ) {
+        return INSTANCE.from( instant );
+    }
+
+    public static Instant stringToInstant( String iso8601ish ) {
+        return INSTANCE.from( iso8601ish );
+    }
+
+    public static String normalizeString( String iso8601ish ) {
+        return INSTANCE.normalize( iso8601ish );
+    }
+
+    public static Instant normalizeInstant( Instant instant ) {
+        return INSTANCE.normalize( instant );
     }
 }
